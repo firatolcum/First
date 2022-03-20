babc7bfc9a6f06ff012ae8e07a5bf53b58f9baa4
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-20 21:16:52 +0300

    Homework 19 add

---
+++ 019_Homework_19-Magic_8_Ball.py
@@ -0,0 +1,59 @@
+
+""""
+Magic 8-Ball
+The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+
+Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+Magic 8-Ball, should I do this project?
+
+We’ll be using the following 9 possible answers for our Magic 8-Ball:
+
+Yes - definitely.
+It is decidedly so.
+Without a doubt.
+Reply hazy, try again.
+Ask again later.
+Better not tell you now.
+My sources say no.
+Outlook not so good.
+Very doubtful.
+The output of the program will have the following format:
+
+[Name] asks: [Question]
+Magic 8-Ball’s answer: [Answer]
+For example:
+
+Joe asks: Is this real life?
+Magic 8-Ball's answer: Better not tell you now
+Let’s get started!
+"""
+
+name = "Fırat"
+question = "Am I a lucky guy?"
+answer = ""
+import random
+random_number = random.randint(1, 9)
+if random_number == 1:
+    answer = "Yes - definitely."
+elif random_number == 2:
+    answer = "It is decidedly so."
+elif random_number == 3:
+    answer = "Without a doubt."
+elif random_number == 4:
+    answer = "Reply, hazy, try again."
+elif random_number == 5:
+    answer = "Ask again later."
+elif random_number == 6:
+    answer = "Better not tell you now."
+elif random_number == 7:
+    answer = "My sources say no."
+elif random_number == 8:
+    answer = "Outlook not so good."
+elif random_number == 9:
+    answer = "Very doubtful."
+else:
+    answer = "Error"
+
+print(name, "asks", question)
+print("Magic 8-Ball's answer:", answer)
