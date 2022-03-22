b0100daa82aa06fecc2b1925d9ef45818b7254d2
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-22 15:16:59 +0300

    Example 3 add

--- Code_Challenges-List_Advanced.py
+++ Code_Challenges-List_Advanced.py
@@ -38,3 +38,26 @@ def remove_middle(lst, start, end):
 
 
 print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+
+"""
+Code Challenge-1 : More Frequent Item
+Let’s go back to our factory example. We have a conveyor belt of items where each item is represented by a different number. We want to know, out of two items, which one shows up more on our belt. To solve this, we can use a function with three parameters. One parameter for the list of items, another for the first item we are comparing, and another for the second item. Here are the steps:
+
+1-Define the function to accept three parameters: the list, the first item, and the second item
+2-Count the number of times item1 shows up in our list
+3-Count the number of times item2 shows up in our list
+4-If item1 shows up more, return item1. Otherwise, return item2
+"""
+
+
+def more_frequent_item(lst, item1, item2):
+    num1 = lst.count(item1)
+    num2 = lst.count(item2)
+    if num1 > num2:
+        return item1
+    else:
+        return item2
+
+
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
