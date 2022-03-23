aec47948a4169651fee9b86acd4e9c148a9f3ad4
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-23 10:10:56 +0300

    Challenge 1 and 2 add

---
+++ Codecademy_Assignments/Code_Challenges/Control_Flow/Challenge_1-Large_Power.py
@@ -0,0 +1,23 @@
+
+"""
+Challenge-1 : Large Power
+For the first code challenge, we are going to create a method that tests whether the result of taking the power of one number to another number provides an answer which is greater than 5000. We will use a conditional statement to return True if the result is greater than 5000 or return False if it is not. In order to accomplish this, we will need the following steps:
+
+1-Define the function to accept two input parameters called base and exponent
+2-Calculate the result of base to the power of exponent
+3-Use an if statement to test if the result is greater than 5000. If it is then return True. Otherwise, return False
+"""
+
+
+def large_power(base, exponent):
+    outcome = base**exponent
+    if outcome > 5000:
+        return True
+    else:
+        return False
+
+
+print(large_power(2, 13))
+# should print True
+print(large_power(2, 12))
+# should print False
