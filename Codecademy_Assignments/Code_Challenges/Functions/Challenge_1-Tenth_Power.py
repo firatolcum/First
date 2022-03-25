5176a3cf05f8c211d5e84e008a1e0cb6f0f455c5
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-25 13:05:23 +0300

    Challenge 1 add

---
+++ Codecademy_Assignments/Code_Challenges/Functions/Challenge_1-Tenth_Power.py
@@ -0,0 +1,15 @@
+"""
+Challenge-1 : Tenth Power
+Let’s create some functions which can help us solve math problems! For this first function, we are going to take the tenth power of a number. In order to do this we need to do three things:
+
+1-Set up the function header for tenth_power which accepts one parameter
+2-Take the tenth power of the input value
+3-Return the result
+"""
+
+
+def tenth_power(num):
+    return num ** 10
+
+
+print(tenth_power(2))  # 1024
