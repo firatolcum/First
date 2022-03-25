7e9d70d0095da0599629c10d65633eb382f062ac
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-25 13:06:45 +0300

    Challenge 2 add

---
+++ Codecademy_Assignments/Code_Challenges/Functions/Challenge_2-Square_Root.py
@@ -0,0 +1,15 @@
+"""
+Challenge-2 : Square Root
+Another useful function for solving math problems is the square root function. We can create this using similar steps from the last problem. The code will look very similar. We need to:
+
+1-Set up the function header for square_root which accepts one parameter
+2-Take the square root of the input value
+3-Return the result
+"""
+
+
+def square_root(num):
+    return num ** 0.5
+
+
+print(square_root(16))  # 4
