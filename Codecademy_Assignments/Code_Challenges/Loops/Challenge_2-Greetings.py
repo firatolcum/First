0f64a748bbc68ce4417a99ff57eb533be0cdf33e
firatolcum <firatolcum@anadolu.edu.tr>
2022-04-01 10:53:24 +0300

    challenge 2 upload

---
+++ Codecademy_Assignments/Code_Challenges/Loops/Challenge_2-Greetings.py
@@ -0,0 +1,20 @@
+"""
+Challenge-2 : Greetings
+You are invited to a social gathering, but you are tired of greeting everyone there. Luckily we can create a function to accomplish this task for us. In this challenge, we will take a list of names and prepend the string 'Hello, ' before each name. This will require a few steps:
+
+1-Define the function to accept a list of strings as a single parameter called names
+2-Create a new list of strings
+3-Loop through each name in names
+4-Within the loop, concatenate 'Hello, ' and the current name together and append this new string to the new list of strings
+5-Afterwards, return the new list of strings
+"""
+
+
+def add_greetings(names):
+    new_list = []
+    for name in names:
+        new_list.append("Hello " + name)
+    return new_list
+
+
+print(add_greetings(["Owen", "Max", "Sophie"]))
